d11c000d928285f6a36ccb8fd223cce552eaa18a
lunarinaa <aydan.akbarova@gmail.com>
2023-10-16 09:26:55 -0700

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char abc = 'z';
+while (abc <= 'a')
+{
+putchar(abc);
+abc--;
+}
+putchar('\n');
+return (0);
+}
