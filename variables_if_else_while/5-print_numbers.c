5b399dd082e780e6b32c08548ff59d840308768c
lunarinaa <aydan.akbarova@gmail.com>
2023-10-16 09:39:38 -0700

    123456789

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("123456789\n");
+return (0);
+}
