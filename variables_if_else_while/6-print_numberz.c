73633d7c8a70b787d60b666d23d163cc2c984704
lunarinaa <aydan.akbarova@gmail.com>
2023-10-16 10:15:30 -0700

    test

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i <=9; i++)
+putchar('0' + i);
+putchar("\n");
+return (0);
+}
