7f890fe18a770b9f68d53a37c9e78cada4bde3dd
lunarinaa <aydan.akbarova@gmail.com>
2023-10-16 07:19:55 -0700

    programm to return last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * main - Entry point
+ * Pogramm to assign random number to the n
+ * Return: 0 (succsess)
+*/
+int main(void)
+{
+int n, digit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is", n);
+digit = n % 10;
+if (digit > 5)
+{
+printf("and is greater than 5\n");
+}
+else if
+(digit != 0 && digit < 6);
+{
+printf("and is less than 6 and not 0\n");
+}
+if (digit == 0)
+{
+printf("and is 0\n");
+}
+return (0);
+}
