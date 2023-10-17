578b6ba492e9e6e54cd4fb58dcb7dee25cc57e5c
lunarinaa <aydan.akbarova@gmail.com>
2023-10-17 10:37:30 -0700

    x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Programm that prints alphabet
+ *Return: 0 (success)
+ */
+int main(void)
+{
+print_alphabet_x10();
+int times;
+for (int i = 0; i < times; i++)
+{
+for (char abc = 'a'; abc <= 'z'; abc++)
+{_putchar (abc);
+}
+_putchar ('\n');
+}
+return (0);
+}
