d290cd59bac106567cf3c05265e25ac97248b11d
lunarinaa <aydan.akbarova@gmail.com>
2023-10-17 10:46:28 -0700

    compile error

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,11 @@ int main(void)
 {
 print_alphabet_x10();
 int times;
-for (int i = 0; i < times; i++)
+int i;
+ for (i = 0; i < times; i++)
 {
-for (char abc = 'a'; abc <= 'z'; abc++)
+char abc;  
+for (abc = 'a'; abc <= 'z'; abc++)
 {_putchar (abc);
 }
 _putchar ('\n');
