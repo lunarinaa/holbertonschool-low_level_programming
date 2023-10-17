1ac84d64f6aa112d9a38c1272bf2bb0d8d27955d
lunarinaa <aydan.akbarova@gmail.com>
2023-10-17 10:11:02 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *Programm that prints alphabet
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar (letter);
+_putchar('\n');
+}
