9f812ec126e88b4fb9265cadc1183dd078ad3c08
lunarinaa <aydan.akbarova@gmail.com>
2023-10-18 07:44:19 -0700

    sum up

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function to sum two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: sum of (a+b)
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
