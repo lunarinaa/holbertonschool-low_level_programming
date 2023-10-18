e8898e71b5ba106ff13c2501310449f766065446
lunarinaa <aydan.akbarova@gmail.com>
2023-10-18 02:08:21 -0700

    indentation fixed

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,14 +5,16 @@
 void jack_bauer(void)
 {
 int h, m;
-for (h = 0; h <= 24; h++)
-{
-for (m = 0; m <= 59; m++);
-{ _putchar ((h / 10) + '0');
-_putchar ((h % 10) + '0');
-_putchar (':');
-_putchar ((m / 10) + '0');
-_putchar ((m % 10) + '0');
+for (h = 0; h <= 23; h++)
+	{
+	for (m = 0; m <= 59; m++)
+		{
+		_putchar ((h / 10) + '0');
+		_putchar ((h % 10) + '0');
+		_putchar (':');
+		_putchar ((m / 10) + '0');
+		_putchar ((m % 10) + '0');
+		_putchar ('\n');
 }
 }
 }
