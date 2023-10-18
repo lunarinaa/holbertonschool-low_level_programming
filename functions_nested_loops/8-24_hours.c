79d09201b4d71228a798098b448aa3ca11eae5a1
lunarinaa <aydan.akbarova@gmail.com>
2023-10-18 01:38:20 -0700

    jack

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * jack_bauer - every minute of jack's day
+ */
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h <= 24; h++)
+{
+for (m = 0; m <= 59; m++);
+{ _putchar ((h / 10) + '0');
+_putchar ((h % 10) + '0');
+_putchar (':');
+_putchar ((m / 10) + '0');
+_putchar ((m % 10) + '0');
+}
+}
+}
