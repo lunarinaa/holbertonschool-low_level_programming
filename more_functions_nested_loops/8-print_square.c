e277ffc058ff3168866938419a86d956f0a5c9ee
lunarinaa <aydan.akbarova@gmail.com>
2023-10-19 03:38:19 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Print square  #.
+ * @size:  size of the square
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+
+			if (b == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
