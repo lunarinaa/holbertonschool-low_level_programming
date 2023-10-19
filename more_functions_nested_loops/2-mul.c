891e8a0d16903471e7f6f53ff2a5e7650be002ef
lunarinaa <aydan.akbarova@gmail.com>
2023-10-19 01:32:06 -0700

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: (0) success
+ */
+int mul(int a, int b)
+{
+int c = a * b;
+_putchar (c + '0');
+return (0);
+}
