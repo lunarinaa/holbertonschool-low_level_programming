1dbb78826ae9ba95a9f2886752eb9dafba1c9504
lunarinaa <aydan.akbarova@gmail.com>
2023-10-26 11:11:05 -0700

    uppercase string

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - function to change all lowercases to uppecase.
+ * @str: string
+ * Return: uppercase str
+ */
+char *string_toupper(char *str)
+{
+int i = 0;
+while (str[i])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+i++;
+}
+return (str);
+}
