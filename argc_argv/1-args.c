aa4a708c96772ed7483408e2e96ff833057057d7
lunarinaa <aydan.akbarova@gmail.com>
2023-11-02 00:58:56 -0700

    number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments passed to the programm
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
