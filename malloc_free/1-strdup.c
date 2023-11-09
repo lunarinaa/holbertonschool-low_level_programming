3c5c23f2cd666dd2f24657b2c2d30e94abfb4f67
lunarinaa <aydan.akbarova@gmail.com>
2023-11-09 06:37:36 -0800

    modified

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,15 +7,29 @@
  */
 char *_strdup(char *str)
 {
-char *copy;
 int i, len = 0;
+char *copy;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
 for (i = 0; str[i]; i++)
+{
 len++;
+}
+
 copy = malloc(sizeof(char) * (len + 1));
 if (copy == NULL)
+{
 return (NULL);
+}
+
 for (i = 0; str[i]; i++)
+{
 copy[i] = str[i];
+}
 copy[len] = '\0';
 return (copy);
 }
