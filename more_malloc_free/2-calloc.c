4887c431423b8d3248c56d818485e2488cf2a1f5
lunarinaa <aydan.akbarova@gmail.com>
2023-11-10 03:16:13 -0800

    set to 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,12 +9,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
+char *zero;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 	return (NULL);
 ptr = malloc(size * nmemb);
 if (ptr == NULL)
 {
 	return (NULL);
+}
+zero = ptr;
+for (i = 0; i < (size * nmemb); i++)
+{
+	zero[i] = 0;
 }
 return (ptr);
 }
