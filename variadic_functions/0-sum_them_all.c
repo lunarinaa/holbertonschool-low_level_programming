3d89be44892c8762a50ea20c5153d999f683bc4b
lunarinaa <aydan.akbarova@gmail.com>
2023-11-23 22:29:14 -0800

    betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,17 +6,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i, sum = 0;
+va_list args;
+unsigned int i, sum = 0;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+	sum += va_arg(args, int);
+}
+va_end(args);
 
-    va_start(args, n);
-
-    for (i = 0; i < n; i++) {
-        sum += va_arg(args, int);
-    }
-
-    va_end(args);
-
-    return (sum);
+return (sum);
 }
 
