891587287f943a92c88480546ee1aff0ff2b5538
lunarinaa <aydan.akbarova@gmail.com>
2023-12-04 03:41:40 -0800

    corrections

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,13 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
-	printf("[0] (nil)\n");
 	while (h != 0)
 	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+	else
+
 		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
+	h = h->next;
+	count++;
 	}
 	return (count);
 }
