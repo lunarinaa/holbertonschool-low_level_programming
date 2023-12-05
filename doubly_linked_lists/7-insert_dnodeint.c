96d23a86d56f0f91037185a5219c521cccde8c06
lunarinaa <aydan.akbarova@gmail.com>
2023-12-05 06:42:17 -0800

    insert at specific position

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insetr_dnodeint_at_index - function to insert node at specific position
+ * @h: pointer to the 1st node
+ * @idx: number of position
+ * @n: value of node
+ *
+ * Return: new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *temp = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+		if (temp == NULL)
+			return (NULL);
+	}
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+	else
+	{
+		newnode = malloc(sizeof(dlistint_t));
+		if (newnode == NULL)
+			return (NULL);
+		newnode->n = n;
+		newnode->prev = temp;
+		newnode->next = temp->next;
+		temp->next->prev = newnode;
+		temp->next = newnode;
+	}
+	return (newnode);
+}
+
