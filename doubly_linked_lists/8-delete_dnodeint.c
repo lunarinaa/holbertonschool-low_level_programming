5dd382d2301581ebaeee6e84f6c7b696622add1d
lunarinaa <aydan.akbarova@gmail.com>
2023-12-07 01:34:10 -0800

    edge cases added

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,15 +11,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL || index <= 0)
+	if (*head == NULL)
 		return (-1);
-	while (i < index)
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+	}
+	while (i < index && temp != NULL)
 	{
 		temp = temp->next;
 		i++;
 	}
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
+	if (temp == NULL)
+		return (-1);
+
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
 	free(temp);
 	return (1);
 }
